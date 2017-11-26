457fd6ebce9aaac39d053689739f49776c99ddd9
Marcin K <mmmanio93@gmail.com>
2017-11-26 13:28:55 +0100

    Failing test added

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     private Calculator calc;
@@ -15,7 +16,7 @@ public class CalculatorTest {
         int a = 0, b = 5;
         int result = calc.add(a, b);
 
-        Assert.assertEquals(result, 5);
+        assertEquals(result, 5);
     }
 
     @Test
@@ -23,7 +24,7 @@ public class CalculatorTest {
         int a = 12, b = 5;
         int result = calc.subtract(a, b);
 
-        Assert.assertEquals(result, 7);
+        assertEquals(result, 7);
     }
 
     @Test
@@ -31,7 +32,7 @@ public class CalculatorTest {
         int a = 0, b = 5;
         int result = calc.multiply(a, b);
 
-        Assert.assertEquals(result, 0);
+        assertEquals(result, 0);
     }
 
 
@@ -40,7 +41,7 @@ public class CalculatorTest {
         int a = 12, b = 6;
         int result = calc.divide(a, b);
 
-        Assert.assertEquals(result, 2);
+        assertEquals(result, 2);
     }
 
     @Test(expected = ArithmeticException.class)
@@ -49,4 +50,11 @@ public class CalculatorTest {
         calc.divide(a, b);
     }
 
+    @Test
+    public void shouldFail() throws Exception {
+        int a = 2, b = 2;
+        int result = calc.add(a, b);
+
+        assertEquals(result, 5);
+    }
 }
